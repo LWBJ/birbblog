074288c3dff3cadb83d524eeea41d0513c8addf0
John Pot <dummy2email77@gmail.com>
2019-10-04 13:38:33 +0800

    git ignore

---
+++ build/precache-manifest.94431948d11762d697eb687200682d5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614859305b25341dbe530353cb1673e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd05955d7f7fac3c07ed",
+    "url": "/static/css/main.499e43bb.chunk.css"
+  },
+  {
+    "revision": "cf6e65b5d422fdb2d86c",
+    "url": "/static/js/2.6c0b8a29.chunk.js"
+  },
+  {
+    "revision": "bd05955d7f7fac3c07ed",
+    "url": "/static/js/main.c9caa933.chunk.js"
+  },
+  {
+    "revision": "631455e57232560fa1f0",
+    "url": "/static/js/runtime-main.80e0a411.js"
+  }
+]);
